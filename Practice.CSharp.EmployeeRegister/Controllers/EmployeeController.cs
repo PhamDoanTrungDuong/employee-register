b79c1fa35a43cf3eb61906e2bb6213ef1486da76
Dương Phạm <pdtduong03@gmail.com>
2021-10-11 14:26:05 +0700

    Add project files.

---
+++ Practice.CSharp.EmployeeRegister/Controllers/EmployeeController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Practice.CSharp.EmployeeRegister.Models;
+
+namespace Practice.CSharp.EmployeeRegister.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public EmployeeController(EmployeeDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            this._env = env;
+        }
+
+        // GET: api/Employee
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmployeeModel>>> GetEmployeeModels()
+        {
+            return await _context.EmployeeModels
+                .Select(x => new EmployeeModel()
+                {
+                    EmployeeID = x.EmployeeID,
+                    EmployeeName = x.EmployeeName,
+                    Occupation = x.Occupation,
+                    ImageName = x.ImageName,
+                    ImageSrc = String.Format("{0}://{1}{2}/Images/{3}",Request.Scheme, Request.Host, Request.PathBase, x.ImageName)
+                })
+                .ToListAsync();
+        }
+
+        // GET: api/Employee/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeModel>> GetEmployeeModel(int id)
+        {
+            var employeeModel = await _context.EmployeeModels.FindAsync(id);
+
+            if (employeeModel == null)
+            {
+                return NotFound();
+            }
+
+            return employeeModel;
+        }
+
+        // PUT: api/Employee/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployeeModel(int id, [FromForm] EmployeeModel employeeModel)
+        {
+            if (id != employeeModel.EmployeeID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employeeModel).State = EntityState.Modified;
+
+            if(employeeModel.ImageFile != null)
+            {
+                DeleteImage(employeeModel.ImageName);
+                employeeModel.ImageName = await SaveImage(employeeModel.ImageFile);
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employee
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<EmployeeModel>> PostEmployeeModel([FromForm]EmployeeModel employeeModel)
+        {
+            employeeModel.ImageName = await SaveImage(employeeModel.ImageFile);
+            _context.EmployeeModels.Add(employeeModel);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(201);
+        }
+
+        // DELETE: api/Employee/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployeeModel(int id)
+        {
+            var employeeModel = await _context.EmployeeModels.FindAsync(id);
+            if (employeeModel == null)
+            {
+                return NotFound();
+            }
+            DeleteImage(employeeModel.ImageName);
+            _context.EmployeeModels.Remove(employeeModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeModelExists(int id)
+        {
+            return _context.EmployeeModels.Any(e => e.EmployeeID == id);
+        }
+
+        [NonAction]
+        public async Task<string> SaveImage(IFormFile imageFile){
+            string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName).Take(10).ToArray()).Replace(' ','-');
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(imageFile.FileName);
+            var imagePath = Path.Combine(_env.ContentRootPath, "Images", imageName);
+            using(var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+            return imageName;
+        }
+
+        [NonAction]
+        public void DeleteImage(string imageName)
+        {
+            var imagePath = Path.Combine(_env.ContentRootPath, "Images", imageName);
+            if (System.IO.File.Exists(imagePath))
+                System.IO.File.Delete(imagePath);
+        }
+    }
+}
